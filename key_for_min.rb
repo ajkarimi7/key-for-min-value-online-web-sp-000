fc216a95b3ba631e74c2c82f89b59121c907579a
Ali Karimi <ajkarimi7@gmail.com>
2019-09-11 03:38:14 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
   name_hash.sort do |name_hash|
-    name_hash.find_by(index[0]) == name_min
+    name_hash.find_by(index[0]) == name_short
   end 
-  name_min
+  name_mi
 end
